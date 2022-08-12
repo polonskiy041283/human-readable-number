ba63610d4b46279d747eeec50bc87b359be936c6
polonskiy041283 <polonskiy041283@gmail.com>
2022-08-13 00:15:41 +0300

    new update

--- src/index.js
+++ src/index.js
@@ -1,80 +1,90 @@
 module.exports = function toReadable (number) {
-let num = 45; 
-let arr = Array.from(num.toString());
-let len = arr.length;
-let num0 = Number(arr[0]);
-let num1 = Number(arr[1]);
-let num2 = Number(arr[2]);
 
-if (len === 1) {
-    console.log(units(num0));
-}
-else if (len === 2) {
-    if (num > 19) {
-        console.log(dozens(num0), units(num1));
+    if (number === 0) return 'zero'
+  
+    const arr10 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const arr100 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const arr11 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  
+    function one(number) {
+      for (let i = 0; i <= 9; i++) {
+        if (number === i) {
+          return arr10[i];
+        }
+      }
+    } 
+  
+    function dozUpTo20(number) {
+      for (let i = 10; i < 20; i++) {
+        if (number === i) {
+          return arr11[i - 10];
+        }
+      }
     }
-    else {
-        console.log(especial(num));
+  
+    function dozMoreThan20(number) {
+      let nStr = String(number);
+      for (let i = 2; i <= 9; i++) {
+        if (Number(nStr[0]) === i) {
+          for (let y = 0; y <= 9; y++) {
+            if (Number(nStr[1]) === 0) { // убираем ПРОБЕЛ ПОСЛЕ десятых
+              return `${arr100[i]}${one(y)}`;
+            }   
+            else if (Number(nStr[1]) === y) {
+              return `${arr100[i]} ${one(y)}`;
+            }
+          }
+        }
+      }
     }
-}
-else if (len === 3){
-    let str = num1.toString() + num2.toString();
-    let separated_num = Number(str);
-
-    if (separated_num > 19) {
-        console.log(units(num0),'hundred', dozens(num1), units(num2));
+  
+    function hundreds(number) {
+      let firstN = Number(String(number)[0]);
+      let restN = Number(String(number).slice(1));
+      if (restN === 0) { // убираем ПРОБЕЛ ПОСЛЕ hundred
+        return `${one(firstN)} hundred${one(restN)}`;  
+      } else if (restN >= 20) {
+          return `${one(firstN)} hundred ${dozMoreThan20(restN)}`;
+      } else if (restN >= 10) {
+          return `${one(firstN)} hundred ${dozUpTo20(restN)}`;
+      } else {
+          return `${one(firstN)} hundred ${one(restN)}`;
+  
+      }
     }
-    else if (separated_num >= 10){
-        console.log(units(num0), 'hundred', especial(separated_num));
+  
+  function last3Digits(num) {
+  
+    if ((String(num)).length === 1) {
+      return one(num);
+    } else if (num < 20 && num >= 10) {
+        return dozUpTo20(num);
+    } else if (num >= 20 && (String(num)).length === 2) {
+        return dozMoreThan20(num);  
+    } else if ((String(num)).length === 3) {
+        return hundreds(num); 
     }
-    else {
-        console.log(units(num0), 'hundred', units(num2));
+  }
+  
+  function human(n) {
+  let l = String(n).length;
+  let first3 = Number(String(n).slice(0, l - 3));
+  let last3 = Number(String(n).slice(l - 3));
+  
+    if (l > 3 && Number(String(first3)[2]) === 0 && Number(String(first3)[1]) != 1) {
+      return `${last3Digits(first3)}thousand ${last3Digits(last3)}`.split(' ').join(' ');
     }
-}
-
-function units(num0) {
-    let res_units = '';
-    switch(num0) {
-        case 1:  res_units += 'one'; break;
-        case 2:  res_units += 'two'; break;
-        case 3:  res_units += 'three'; break;
-        case 4:  res_units += 'four'; break;
-        case 5:  res_units += 'five'; break;
-        case 6:  res_units += 'six'; break;
-        case 7:  res_units += 'seven'; break;
-        case 8:  res_units += 'eight'; break;
-        case 9:  res_units += 'nine'; break;
+    else if (l > 3) {
+      return `${last3Digits(first3)} thousand ${last3Digits(last3)}`.split(' ').join(' ');
+    } else if (l === 3) {
+        return hundreds(n)
+    } else if (l === 1) {
+        return one(n)
+    } else if (n >= 10 && n < 20) {
+      return dozUpTo20(n)
+    } else {
+      return dozMoreThan20(n);
     }
-    return res_units;
-}
-function dozens(num0) {
-    let res_dozens = '';
-    switch(num0) {
-        case 2:  res_dozens += 'twenty'; break;
-        case 3:  res_dozens += 'thirty'; break;
-        case 4:  res_dozens += 'fourty'; break;
-        case 5:  res_dozens += 'fifty'; break;
-        case 6:  res_dozens += 'sixty'; break;
-        case 7:  res_dozens += 'seventy'; break;
-        case 8:  res_dozens += 'eighty'; break;
-        case 9:  res_dozens += 'ninety'; break;
-    }
-    return res_dozens;
-}
-function especial(num) {
-    let res_especial = '';
-    switch(num) {
-        case 10:  res_especial += 'ten'; break;
-        case 11:  res_especial += 'eleven'; break;
-        case 12:  res_especial += 'twelve'; break;
-        case 13:  res_especial += 'thirteen'; break;
-        case 14:  res_especial += 'fourteen'; break;
-        case 15:  res_especial += 'fifteen'; break;
-        case 16:  res_especial += 'sixteen'; break;
-        case 17:  res_especial += 'seventeen'; break;
-        case 18:  res_especial += 'eightteen'; break;
-        case 19:  res_especial += 'nineteen'; break;
-    }
-    return res_especial;
-}
-}
+  }
+  return human(number);
+  }
